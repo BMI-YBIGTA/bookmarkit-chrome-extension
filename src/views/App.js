d9be8fdf18c559e8afd4294ef8647728a3f35f9d
Imsung_You <isyou7487@naver.com>
2021-12-19 18:44:20 +0900

    [feat] 로그인/회원가입 뼈대 구성

--- src/views/App.js
+++ src/views/App.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Header } from "semantic-ui-react";
 import {
-  accountAuth, accountProfile, accountLogout
-} from '../actions/account';
-import { setEnabled, setStats } from '../actions/marker';
-import Authed from './Authed';
-import Unauthed from './Unauthed';
-import './App.css';
+  accountAuth,
+  accountProfile,
+  accountLogout,
+} from "../../actions/account";
+import { setEnabled, setStats } from "../../actions/marker";
+import Authed from "./Authed";
+import Unauthed from "./Unauthed";
+import "./App.css";
 
 class App extends Component {
   render() {
     const { token } = this.props;
-    // const View = token? Authed : Unauthed;
-    const View = Authed;
+    const View = token ? Authed : Unauthed;
+    //const View = Authed;
     return (
-      <div className='App'>
-        <Header as='h3' attached='top' textAlign='center' inverted color='teal'>
+      <div className="App">
+        <Header as="h3" attached="top" textAlign="center" inverted color="teal">
           Bookmarkit
         </Header>
-        <div className='App-view'>
-          <View {...this.props}/>
+        <div className="App-view">
+          <View {...this.props} />
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => Object.assign(
-  {}, state.account, state.marker
-);
+const mapStateToProps = (state) =>
+  Object.assign({}, state.account, state.marker);
 
-const mapDispatchToProps = dispatch => ({
-  accountAuth: data => {
+const mapDispatchToProps = (dispatch) => ({
+  accountAuth: (data) => {
     dispatch(accountAuth(data));
   },
-  accountProfile: data => {
+  accountProfile: (data) => {
     dispatch(accountProfile(data));
   },
   accountLogout: () => {
     dispatch(accountLogout());
   },
-  setEnabled: data => {
+  setEnabled: (data) => {
     dispatch(setEnabled(data));
   },
-  setStats: data => {
+  setStats: (data) => {
     dispatch(setStats(data));
-  }
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
