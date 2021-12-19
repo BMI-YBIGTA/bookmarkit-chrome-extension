a36966e462f8bc03b6c4406803b742f307cfce3f
Woojin Joo <jwjin95@yonsei.ac.kr>
2021-12-19 21:29:46 +0900

    [feat] synchronize bookmarks

--- src/views/Authed.js
+++ src/views/Authed.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Container, Segment, Button, Checkbox, Header, Icon, Label, Placeholder
+  Container, Segment, Button, Checkbox, Header, Icon, Label, Placeholder, Modal
 } from 'semantic-ui-react';
 import Register from './Register';
 
@@ -18,23 +18,62 @@ export default class Authed extends Component {
     !checked && setStats(false);
   }
 
-  onRegister = (e) => {
+  onSync = (e) => {
     e.preventDefault();
-    return (
-      <div>
-        <Register />
-      </div>
-    )
+    this.BookmarkService.getListAboutTree().then(console.log);
+    this.setState({
+      open: true
+    })
   }
+
+  callModal = () => {
+    
+      SyncModal()
+      console.log()
+  }
+
+
+
+  BookmarkService = Object.freeze({
+
+    /** 북마크 트리를 가져옵니다. **/
+    getTree () {
+      return new Promise(resolve => {
+        chrome.bookmarks.getTree(([ tree ]) => resolve(tree));
+      })
+    },
+  
+    /** 북마크의 트리를 목록으로 변환하여 가져옵니다. **/
+    async getListAboutTree () {
+      const tree = await this.getTree()
+      let bookmarks = tree.children.flatMap(v => v.children);
+      while (bookmarks.find(v => v.children)) {
+        bookmarks = bookmarks.flatMap(v => v.children || [ v ])
+      }
+      return bookmarks.map(({ id, title, url }) => ({ id, title, url }));
+    },
+  });
+
 
   render () {
     const { name, keywords, enabled, stats } = this.props;
+    const { open } = this.state;
     return (
       <div>
 
         <Container textAlign='center'>
           <Button floated='right' circular icon='sign out' onClick={this.onLogout} />
-          <Button onClick={this.onRegister}>북마크 등록</Button>
+          <Button>북마크 등록</Button>
+          <Button className='sync' onClick={this.onSync}>북마크 동기화</Button>
+          <Modal
+            header='Reminder!'
+            content='Call Benjamin regarding the reports.'
+            actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
+            open={open}
+            onOpen={()=>this.setState({open:true})}
+            onClose={()=>this.setState({open:false})}
+          />
+      
         </Container>
 
       </div>
