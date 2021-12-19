b37d62736b0ec079002076afbb12091881f7e948
2hwayoung <hwa3270@gmail.com>
2021-12-20 01:44:28 +0900

    [fix] divide the page components

--- src/views/Authed.js
+++ src/views/Authed.js
@@ -1,70 +1,20 @@
-import React, { Component, useState } from 'react';
-import {
-  Container, Segment, Button, Checkbox, Header, Icon, Label, Placeholder
-} from 'semantic-ui-react';
+import React, { useState } from 'react';
 import Register from './Register';
-
-const BookmarkService = Object.freeze({
-
-  /** 북마크 트리를 가져옵니다. **/
-  getTree () {
-    return new Promise(resolve => {
-      chrome.bookmarks.getTree(([ tree ]) => resolve(tree));
-    })
-  },
-
-  /** 북마크의 트리를 목록으로 변환하여 가져옵니다. **/
-  async getListAboutTree () {
-    const tree = await this.getTree()
-    let bookmarks = tree.children.flatMap(v => v.children);
-    while (bookmarks.find(v => v.children)) {
-      bookmarks = bookmarks.flatMap(v => v.children || [ v ])
-    }
-    return bookmarks.map(({ id, title, url }) => ({ id, title, url }));
-  },
-});
+import Home from './Home';
 
 const Authed = (props) => {
-
-  const { name, keywords, enabled, stats } = props;
+  // console.log(props);
+  const { name, keywords, enabled, stats, pageName } = props;
   const [state, setState] = useState({open: false});
 
-  const onLogout = (e) => {
-    e.preventDefault();
-    const {accountLogout} = props;
-    accountLogout();
+  switch (pageName) {
+    case "Home":
+      return <Home {...props} />;
+    case "Register": 
+      return <Register {...props} />;
+    default:
+      return <div></div>;
   }
-
-  const onCheck = (e, { checked }) => {
-    e.preventDefault();
-    const {setEnabled, setStats} = props;
-    setEnabled(checked);
-    !checked && setStats(false);
-  }
-
-  const onSync = (e) => {
-    e.preventDefault();
-    BookmarkService.getListAboutTree().then(console.log);
-    setState({
-      open: true
-    })
-  }
-
-  const onAdd = (e) => {
-    e.preventDefault();
-
-  }
-
-  return (
-    <div>
-      <Container textAlign='center'>
-        <Button floated='right' circular icon='sign out' onClick={onLogout} />
-        <Button onClick={onAdd}>북마크 등록</Button>
-        <Button className='sync' onClick={onSync}>북마크 동기화</Button>
-      </Container>
-    </div>
-  );
 }
-
 
 export default Authed;
---
+++ src/views/Home.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+  Container, Button
+} from 'semantic-ui-react';
+
+const BookmarkService = Object.freeze({
+
+  /** 북마크 트리를 가져옵니다. **/
+  getTree () {
+    return new Promise(resolve => {
+      chrome.bookmarks.getTree(([ tree ]) => resolve(tree));
+    })
+  },
+
+  /** 북마크의 트리를 목록으로 변환하여 가져옵니다. **/
+  async getListAboutTree () {
+    const tree = await this.getTree()
+    let bookmarks = tree.children.flatMap(v => v.children);
+    while (bookmarks.find(v => v.children)) {
+      bookmarks = bookmarks.flatMap(v => v.children || [ v ])
+    }
+    return bookmarks.map(({ id, title, url }) => ({ id, title, url }));
+  },
+});
+
+const Home = (props) => {
+  console.log(props);
+  // const { name, keywords, enabled, stats, pageName } = props;
+  const [state, setState] = useState({open: false});
+
+  const onLogout = (e) => {
+    e.preventDefault();
+    const {accountLogout} = props;
+    accountLogout();
+  }
+
+  // const onCheck = (e, { checked }) => {
+  //   e.preventDefault();
+  //   const {setEnabled, setStats} = props;
+  //   setEnabled(checked);
+  //   !checked && setStats(false);
+  // }
+
+  const onSync = (e) => {
+    e.preventDefault();
+    BookmarkService.getListAboutTree().then(console.log);
+    setState({
+      open: true
+    })
+  }
+
+  const onAdd = (e) => {
+    e.preventDefault();
+    const {setPageName} = props;
+    setPageName("Register");
+  }
+
+  return (
+    <div>
+      <Container textAlign='center'>
+        <Button floated='right' circular icon='sign out' onClick={onLogout} />
+        <Button onClick={onAdd}>북마크 등록</Button>
+        <Button className='sync' onClick={onSync}>북마크 동기화</Button>
+      </Container>
+    </div>
+  );
+}
+
+
+export default Home;
