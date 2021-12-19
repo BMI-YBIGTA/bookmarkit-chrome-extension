b62d71774d108981ef38de24a806059d0a6853eb
2hwayoung <hwa3270@gmail.com>
2021-12-19 17:59:43 +0900

    [fix] delete options and change structure

--- src/views/Authed.js
+++ src/views/Authed.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import {
   Container, Segment, Button, Checkbox, Header, Icon, Label, Placeholder
 } from 'semantic-ui-react';
+import Register from './Register';
 
 export default class Authed extends Component {
-  onSettings = (e) => {
-    e.preventDefault();
-    chrome.runtime.openOptionsPage && chrome.runtime.openOptionsPage();
-  }
-
   onLogout = (e) => {
     e.preventDefault();
     const {accountLogout} = this.props;
@@ -22,15 +18,23 @@ export default class Authed extends Component {
     !checked && setStats(false);
   }
 
+  onRegister = (e) => {
+    e.preventDefault();
+    return (
+      <div>
+        <Register />
+      </div>
+    )
+  }
+
   render () {
     const { name, keywords, enabled, stats } = this.props;
     return (
       <div>
 
         <Container textAlign='center'>
-          <Button floated='left' circular icon='cog' onClick={this.onSettings} />
           <Button floated='right' circular icon='sign out' onClick={this.onLogout} />
-          <Button>북마크 등록</Button>
+          <Button onClick={this.onRegister}>북마크 등록</Button>
         </Container>
 
       </div>
